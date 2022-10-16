b931d7b5d77d4cb037f4e1c6ad80f49e31949897
Pâmela Freire <113552156+EloisePR@users.noreply.github.com>
2022-10-16 17:00:12 -0300

    Desenvolvendo HTML e CSS do site
    Desenvolvendo a base do site, dando alguns estilos a página e adicionando um pouco de JS

---
+++ js/main.js
@@ -0,0 +1,16 @@
+const selecione = document.querySelector(".select");
+const incremento = document.querySelector(".chave-container");
+const btn = document.querySelector("button");
+const radiobtn = document.querySelector(".radio-button");
+const codificar = document.querySelector("#codificar");
+const decodificar = document.querySelector("#decodificar");
+
+// Incremento da Cifra de César
+selecione.addEventListener("click", function () {
+    if (selecione.value == "cifra") {
+      incremento.style.display = "block";
+    } else {
+      incremento.style.display = "none";
+    }
+});
+
